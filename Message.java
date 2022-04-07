05266d134c73eb9657877608ed70bcc51fe356fd
anjum1993 <99728693+anjum1993@users.noreply.github.com>
2022-04-07 16:25:21 +0530

    SecondCommit

--- Message.java
+++ Message.java
@@ -1,5 +1,11 @@
 class Message
 {
+  
+  String add()
+  {
+    System.out.println("Add component");
+  }
+  
   public static void main(String args[])
   {
     System.out.println("Message module");
